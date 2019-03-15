3348da2ea4a5fd5e4531cf20d181bcbf129f53c1
Andrey <andriocactus@gmail.com>
2019-03-15 21:51:10 +0500

    Добавил вычислительный сервер. Добавил класс для симуляции нагрузки метода. Добавил механизм выполнения методов с симуляцией нагрузки.

---
+++ Shared/CalculateLineData.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Shared
+{
+    [DataContract]
+    public class CalculateLineData
+    {
+        [DataMember]
+        public List<Line> Lines;
+
+        [DataMember]
+        public Plane Plane;
+    }
+
+    /// <summary>
+    ///     Заглушка для самолета.
+    /// </summary>
+    [DataContract]
+    public class Plane
+    {
+    }
+
+    [DataContract]
+    public class Line
+    {
+        [DataMember]
+        public int Nmber;
+
+        [DataMember]
+        public tState State;
+    }
+
+    // ReSharper disable once InconsistentNaming
+    public enum tState
+    {
+        /// <summary>
+        ///     Свободна.
+        /// </summary>
+        Free = 1,
+
+        /// <summary>
+        ///     Занята.
+        /// </summary>
+        Busy = 2
+    }
+}
---
+++ Shared/MethodsPull.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Shared
+{
+     /// <summary>
+     /// Пул симуляционных методов.
+     /// </summary>
+    public class MethodsPull
+    {
+        private List<BaseSimulatedMethod> methods;
+
+        public MethodsPull()
+        {
+            methods = new List<BaseSimulatedMethod>();
+        }
+
+        public object ExecuteMethod(BaseSimulatedMethod bsm)
+        {
+            methods.Add(bsm);
+            Thread.Sleep(1000 * bsm.Time);
+            methods.Remove(bsm);
+            return bsm.Process();
+        }
+
+        /// <summary>
+        /// Возвращает сумму нагрузок выполняемых методов.
+        /// </summary>
+        public int GetLoading()
+        {
+            return methods.Sum(x => x.Loading);
+        }
+    }
+}
